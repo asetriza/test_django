ba14d7fce22d514c0044b41c1a5d166fbe0e216e
asetriza <57208705+asetriza@users.noreply.github.com>
2020-11-07 15:15:07 +0600

    Update processing.py

--- src/aggregation/processing.py
+++ src/aggregation/processing.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from datetime import date
+from datetime import date, datetime
 
 from bson.json_util import dumps, loads
 
@@ -16,12 +16,16 @@ data = db.test_data
 
 def processing_flghts():
     for i in directions.find({}):
-        date_parsed = date.today()
+        now = datetime.now()
+        time_parsed = now.strftime("%H:%M:%S")
+        today_date = date.today()
+        date_parsed = str(today_date.strftime("%d/%m/%Y")).replace("-", "/")
         fly_from = i["fly_from"]
         fly_to = i["fly_to"]
         flights = flights_all_get(fly_from, fly_to, None, None)
         flights_all = {
             "date_parsed": f"{date_parsed}",
+            "time parsed": f"{time_parsed}",
             "fly_from": f"{fly_from}",
             "fly_to": f"{fly_to}",
             "flights": flights,
