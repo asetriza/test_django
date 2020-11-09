1256282eb12faa5acdca740b3f1194d91b6db22a
asetriza <57208705+asetriza@users.noreply.github.com>
2020-11-09 18:55:25 +0600

    Update aggregation_cron.py

--- src/cron/aggregation_cron.py
+++ src/cron/aggregation_cron.py
@@ -9,13 +9,13 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job("cron", month="*", day_of_week="*", hour=18, minute=53, second=1)
+@sched.scheduled_job("cron", month="*", day_of_week="*", hour=12, minute=59, second=1)
 def scheduled_job():
     processing_flghts()
     print("This job processes flghts and sends to db every day at 00:00:01")
 
 
-@sched.scheduled_job("cron", month="*", day_of_week="*", hour=18, minute=53, second=1)
+@sched.scheduled_job("cron", month="*", day_of_week="*", hour=12, minute=59, second=1)
 def scheduled_job():
     print("This job is run every weekday at 5pm.")
 
